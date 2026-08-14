6a001a2dfa512c72b87ef610a2ec4bc1f9ea7b18
CountTails <74679703+CountTails@users.noreply.github.com>
2026-08-14 15:03:13 -0700

    Init: first commit with basic configuration customizing terminal appearance

---
+++ wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.initial_cols = 120
+config.initial_rows = 28
+
+config.font_size = 16
+config.font = wezterm.font("ComicShannsMono Nerd Font Mono")
+config.color_scheme = "Catppuccin Mocha"
+
+return config
