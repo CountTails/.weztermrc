47d93b7f0eda0ec15a3dd9dcf0c5861779f98db3
CountTails <74679703+CountTails@users.noreply.github.com>
2026-08-17 13:51:08 -0700

    Overrides: change the font size and font family used

--- wezterm.lua
+++ wezterm.lua
@@ -5,8 +5,9 @@ local config = wezterm.config_builder()
 config.initial_cols = 120
 config.initial_rows = 28
 
-config.font_size = 16
-config.font = wezterm.font("ComicShannsMono Nerd Font Mono")
+config.font_size = 20
+
+config.font = wezterm.font("AnnotationM Nerd Font Mono")
 config.color_scheme = "Catppuccin Mocha"
 
 return config
