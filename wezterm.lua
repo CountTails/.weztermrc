fcdf3ab1065ebe41dc5018ca3cf84e300aef6a06
CountTails <74679703+CountTails@users.noreply.github.com>
2026-08-20 12:34:12 -0700

    Font option: update the font family used by wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,7 @@ local config = require("utils.init")
 
 return config
 	:init()
-	:with(require("config.typography"):new(20, "AnnotationM Nerd Font Mono"))
+	:with(require("config.typography"):new(20, "CommitMono Nerd Font Mono"))
 	:with(require("config.colorscheme"):new("Catppuccin Mocha"))
 	:with(require("config.cursor"):new("BlinkingBar", 750, "Ease", "Linear"))
 	:configure()
