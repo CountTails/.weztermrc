32ef9fbdb64a942b3ea1f4937551dfc8da480ddf
CountTails <74679703+CountTails@users.noreply.github.com>
2026-09-04 13:12:43 -0700

    options(typography): raw font options are an annotated data table only

--- lua/options/typography/fonts.lua
+++ lua/options/typography/fonts.lua
@@ -1,33 +1,49 @@
 local wezterm = require("wezterm")
-local platform = require("utils.platform")
 
-local M = {}
+---@class RawFontOptions
+---@field desired_fonts string[] list of fonts to try to resolve for terminal text
+---@field base_font_size integer the base font size to use for the wezterm configuration
+---@field windows_font_dirs string[] list of directories to lookup fonts on a Windows system
+---@field macos_font_dirs string[] list of directories to lookup fonts on a macOS system
+---@field linux_font_dirs string[] list of directories to lookup fonts on a Linux system
+---@field font_locator string rule for determining how fonts are located and loaded
+local RawFontOptions = {}
+
+--- EDIT this value to adjust the base size
+---@type integer the base size font to use for the wezterm configuration
+RawFontOptions.base_font_size = 18
 
 --- EDIT this value to adjust the fonts to choose from
 ---@type string[] list of fonts to try to resolve for terminal text
-M.fonts = {
+RawFontOptions.desired_fonts = {
 	"CommitMono Nerd Font Mono",
 	"Hurmit Nerd Font Mono",
 }
 
---- EDIT this value to adjust the base size
----@type integer the base size font to use for the wezterm configuration
-M.base_size = 16
+--- EDIT this value to adjust the rule for locating and loading fonts
+---@type "ConfigDirsOnly"|nil
+RawFontOptions.font_locator = "ConfigDirsOnly"
 
----@type string[] list of directories to lookup fonts from
-M.font_locations = {}
+--- EDIT this value to adjust where fonts are looked up on Windows systems
+---@type string[]
+RawFontOptions.windows_font_dirs = {
+	"C:\\Windows\\Fonts",
+}
 
---- EDIT the `table.insert` lines to add/remove/modify the directories that are used for font resolution
-if platform.is_mac() then
-	table.insert(M.font_locations, "/System/Library/Fonts")
-	table.insert(M.font_locations, "/Library/Fonts")
-	table.insert(M.font_locations, wezterm.home_dir .. "/Library/Fonts")
-elseif platform.is_linux() then
-	table.insert(M.font_locations, "/usr/share/fonts")
-	table.insert(M.font_locations, "/usr/local/share/fonts")
-	table.insert(M.font_locations, wezterm.home_dir .. "/.local/share/fonts")
-elseif platform.is_win() then
-	table.insert(M.font_locations, "C:\\Windows\\Fonts")
-end
+--- EDIT this value to adjust where fonts are looked up on macOS systems
+---@type string[]
+RawFontOptions.macos_font_dirs = {
+	"/System/Library/Fonts",
+	"/Library/Fonts",
+	wezterm.home_dir .. "/Library/Fonts",
+}
 
-return M
+--- EDIT this value to adjust where fonts are looked up on Linux systems
+---@type string[]
+RawFontOptions.linux_font_dirs = {
+	"/usr/share/fonts",
+	"/usr/local/share/fonts",
+	wezterm.home_dir .. "/.local/share/fonts",
+}
+
+return RawFontOptions
