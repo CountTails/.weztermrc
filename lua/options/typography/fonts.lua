6509d1ec2de0d905203fe932b896261f6c8b70fc
CountTails <74679703+CountTails@users.noreply.github.com>
2026-09-14 17:01:59 -0700

    options(typography-and-environment): clean up display options and add required information for responsive font size calculations

--- lua/options/typography/fonts.lua
+++ lua/options/typography/fonts.lua
@@ -1,28 +1,45 @@
 local wezterm = require("wezterm")
 
 ---@class RawFontOptions
----@field desired_fonts string[] list of fonts to try to resolve for terminal text
+---@field terminal_font string the font name to use for terminal applications
+---@field decorative_font string the font name to use for decorative functions
 ---@field base_font_size integer the base font size to use for the wezterm configuration
+---@field base_display_width integer the reference display width for the base font size
+---@field min_font_size_allowed integer the minimum size the font should be allowed to shrink to
+---@field max_font_size_allowed integer the maximum size the font should be allowed to grow to
 ---@field windows_font_dirs string[] list of directories to lookup fonts on a Windows system
 ---@field macos_font_dirs string[] list of directories to lookup fonts on a macOS system
 ---@field linux_font_dirs string[] list of directories to lookup fonts on a Linux system
----@field font_locator string rule for determining how fonts are located and loaded
+---@field use_system_fonts_only boolean rule for determining how fonts are located and loaded
 local RawFontOptions = {}
 
 --- EDIT this value to adjust the base size
 ---@type integer the base size font to use for the wezterm configuration
 RawFontOptions.base_font_size = 18
 
---- EDIT this value to adjust the fonts to choose from
----@type string[] list of fonts to try to resolve for terminal text
-RawFontOptions.desired_fonts = {
-	"CommitMono Nerd Font Mono",
-	"Hurmit Nerd Font Mono",
-}
+--- EDIT this value to adjust the reference display width
+---@type integer the reference display width
+RawFontOptions.base_display_width = 1440
+
+--- EDIT this value to adjust the min font size
+---@type integer smallest font size allowed
+RawFontOptions.min_font_size_allowed = 14
+
+--- EDIT this value to adjust the max font size
+---@type integer largest font size allowed
+RawFontOptions.max_font_size_allowed = 24
+
+--- EDIT this value to adjust the font family used within wezterm panes
+---@type string the font family to use for terminal applications
+RawFontOptions.terminal_font = "CommitMono Nerd Font Mono"
+
+--- EDIT this value to adjust the font family used for wezterm decorative purposes
+---@type string the font family to use for decorative functions
+RawFontOptions.decorative_font = "Hurmit Nerd Font Mono"
 
 --- EDIT this value to adjust the rule for locating and loading fonts
----@type "ConfigDirsOnly"|nil
-RawFontOptions.font_locator = "ConfigDirsOnly"
+---@type boolean true means that font dirs (specified below) will be ignored; false means to exclusively used the dirs specified below
+RawFontOptions.use_system_fonts_only = false
 
 --- EDIT this value to adjust where fonts are looked up on Windows systems
 ---@type string[]
