6f9a837fbde1a4e7ad8fe902c626cf03233042cd
CountTails <74679703+CountTails@users.noreply.github.com>
2026-09-16 16:22:52 -0700

    init(wezterm): connect the static -> effective -> active pipeline for color configuration

--- lua/runtime/setup.lua
+++ lua/runtime/setup.lua
@@ -1,4 +1,5 @@
 local ActiveFontOpts = require("runtime.typography.fonts")
+local ActiveColorSchemeOpts = require("runtime.window.colorscheme")
 
 local M = {}
 
@@ -6,6 +7,12 @@ local M = {}
 ---@return ActiveFontOptions
 M.custom_font_settings = function()
 	return ActiveFontOpts:from_effective_table(require("effective.typography.fonts"))
+end
+
+--- Creates the `ActiveColorSchemeOptions` derived from the effective color scheme table
+---@return ActiveColorSchemeOptions
+M.custom_color_scheme = function()
+	return ActiveColorSchemeOpts:from_effective_table(require("effective.window.colorscheme"))
 end
 
 return M
--- lua/wezterm.lua
+++ lua/wezterm.lua
@@ -3,4 +3,4 @@ require("events")
 local config = require("utils.init")
 local settings = require("runtime.setup")
 
-return config:init():with(settings.custom_font_settings()):configure()
+return config:init():with(settings.custom_font_settings()):with(settings.custom_color_scheme()):configure()
