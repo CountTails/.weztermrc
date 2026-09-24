41e87d408ff4d48f52939ee8e5e2430dd1613a5b
CountTails <74679703+CountTails@users.noreply.github.com>
2026-09-24 14:10:27 -0700

    effective(colors): effective colors distinguish between option to use builtin schemes and custom schemes and resolve appropriately

--- lua/effective/window/colorscheme.lua
+++ lua/effective/window/colorscheme.lua
@@ -19,6 +19,17 @@ end
 
 --- Resolves the effective loadout from the internal raw source table
 function EffectiveColorSchemeOptions:resolve()
+	if self._raw.using_builtin_schemes then
+		self.active_scheme = self:choose_from_builtins()
+	else
+		self.active_scheme = self:choose_from_custom_definitions()
+	end
+end
+
+--- Resolve the effective colorsheme from the builtin list
+---@return table<string, table<string>|string|table<integer, string>|table<string,table<string,string>>>
+---@private
+function EffectiveColorSchemeOptions:choose_from_builtins()
 	local available_schemes = wezterm.color.get_builtin_schemes()
 	local effective_scheme = platform.theme.is_light_mode() and self._raw.light_mode_theme or self._raw.dark_mode_theme
 
@@ -28,7 +39,16 @@ function EffectiveColorSchemeOptions:resolve()
 		error(err)
 	end
 
-	self.active_scheme = available_schemes[effective_scheme]
+	return available_schemes[effective_scheme]
+end
+
+--- Resolve the effective colorscheme utilizing the raw palletes/scheme definitions
+---@return table<string, table<string>|string|table<integer, string>|table<string,table<string,string>>>
+---@private
+function EffectiveColorSchemeOptions:choose_from_custom_definitions()
+	local effective_scheme = platform.theme.is_light_mode() and self._raw.light_mode_scheme
+		or self._raw.dark_mode_scheme
+	return effective_scheme:spec()
 end
 
 local _resolved = EffectiveColorSchemeOptions:from_raw(raw_color_opts)
